4c80980b3789270f5808f7655792052358055112
SERGIO HENRIQUE COIMBRA MIRANDA <sergiogbrox@gmail.com>
2019-03-30 00:56:18 -0300

    teste primeiro loop

--- Testes/teste.py
+++ Testes/teste.py
@@ -1,1 +1,14 @@
-print("Geek University")
+nota = -1
+
+while nota < 0 or nota > 10:
+
+    try:
+        nota = int(input('Digite uma nota de 0 a 10:\n'))
+        if nota < 0 or nota > 10:
+            print('Erro: Numero invalido.\n')
+
+    except ValueError:
+        print('Erro: Numero invalido.\n')
+        continue
+else:
+    print(f'A nota é: {nota}')
